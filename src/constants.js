eebf91a46410d6bb260c68ebd9263467b81a2cce
Adam <trzcinski.adam@gmail.com>
2018-02-23 22:04:15 +0100

    Added remaining planets. Proper planets data.  Second idea on animaton interpolations

--- src/constants.js
+++ src/constants.js
@@ -1,30 +1,62 @@
-export const EARTH_DAY = 2000
-
-export const EARTH_YEAR = 10 * EARTH_DAY
 // export const EARTH_YEAR = 360 * EARTH_DAY
-
+const RADIUS_UNIT = 0.1 // 1000 km
+const AU = 30
+const DAY = 2000 //ms
+const YEAR = 10 * DAY
 /**
  * ALL DISTANCES IN AU UNIT
  */
 
-const EARTH_SEMI_MAJOR_AXIS = 1
-const EARTH_RADIUS = 6371
+export const SUN_RADIUS = 50 * RADIUS_UNIT
 
-const VENUS_AXIAL_TILT = 2.6
-const VENUS_RADIUS = 6000
+export const MERCURY_AXIS = 0.39 * AU
+export const MERCURY_RADIUS = 2.4 * RADIUS_UNIT
+export const MERCURY_DAY = 58.65 * DAY
+export const MERCURY_YEAR = 0.24 * YEAR
+export const MERCURY_INCLINATION = 7 //deg
 
-const MERCURY_AXIAL_TILT = 0
-const MERCURY_INCLINATION = 7
-const MERCURY_RADIUS = 2440
+export const VENUS_AXIS = 0.72 * AU
+export const VENUS_RADIUS = 6 * RADIUS_UNIT
+export const VENUS_DAY = 243 * DAY
+export const VENUS_YEAR = 0.62 * YEAR
+export const VENUS_INCLINATION = 3.4
 
-const MARS_AXIAL_TILT = 25
-const MARS_INCLINATION = 2
-const MARS_RADIUS = 3390
+export const EARTH_AXIS = 1 * AU
+export const EARTH_RADIUS = 6.3 * RADIUS_UNIT
+export const EARTH_DAY = DAY
+export const EARTH_YEAR = YEAR
+export const EARTH_INCLINATION = 7.2
 
-const JUPITER_SEMI_MAJOR_AXIS = 5.2
-const JUPITER_INCLINATION = 1.3
-const JUPITER_RADIUS = 69900
+export const MARS_AXIS = 1.52 * AU
+export const MARS_RADIUS = 3.4 * RADIUS_UNIT
+export const MARS_DAY = 1.03 * DAY
+export const MARS_YEAR = 2.13 * YEAR
+export const MARS_INCLINATION = 2
 
-const SATURN_SEMI_MAJOR_AXIS = 9.58
-const SATURN_INCLINATION = 1.3
-const SATURN_RADIUS = 69900
+export const JUPITER_AXIS = 2.6 * AU
+// export const JUPITER_AXIS = 5.2 * AU
+export const JUPITER_RADIUS = 69.9 * RADIUS_UNIT
+export const JUPITER_YEAR = 11.86 * YEAR
+export const JUPITER_DAY = 10 / 24 * DAY
+export const JUPITER_INCLINATION = 1.3
+
+// export const SATURN_AXIS = 9.58 * AU
+export const SATURN_AXIS = 4.7 * AU
+export const SATURN_RADIUS = 58.2 * RADIUS_UNIT
+export const SATURN_YEAR = 29.46 * YEAR
+export const SATURN_DAY = 11 / 24 * DAY
+export const SATURN_INCLINATION = 2.5
+
+// export const URANUS_AXIS = 19.22 * AU
+export const URANUS_AXIS = 7 * AU
+export const URANUS_RADIUS = 25.4 * RADIUS_UNIT
+export const URANUS_YEAR = 84 * YEAR
+export const URANUS_DAY = -0.72 * DAY
+export const URANUS_INCLINATION = 0.77
+
+export const NEPTUNE_AXIS = 10 * AU
+// export const NEPTUNE_AXIS = 30.11 * AU
+export const NEPTUNE_RADIUS = 24.6 * RADIUS_UNIT
+export const NEPTUNE_YEAR = 164.8 * YEAR
+export const NEPTUNE_DAY = 0.67 * DAY
+export const NEPTUNE_INCLINATION = 1.77
--- src/index.js
+++ src/index.js
@@ -1,107 +1,125 @@
-import * as React from 'react';
-import { View, Scene, PointLight, Animated } from 'react-vr';
+import * as React from 'react'
+import { View, Scene, PointLight, Animated } from 'react-vr'
 
-const Galaxy = Animated.createAnimatedComponent(Scene);
+const Galaxy = Animated.createAnimatedComponent(Scene)
 
-const RCTDeviceEventEmitter = require('RCTDeviceEventEmitter');
+const RCTDeviceEventEmitter = require('RCTDeviceEventEmitter')
 
-import Planet from './Planet';
-import Sun from './Sun';
+import Planet from './Planet'
+import Sun from './Sun'
 
-import { EARTH_YEAR, EARTH_DAY } from './constants';
+import * as constants from './constants'
 
 class App extends React.Component {
-  state = {
-    zoomedIn: false,
-    zoom: new Animated.Value(0),
-  };
-  componentDidMount() {
-    RCTDeviceEventEmitter.addListener('ondblclick', this.handleDbClick);
-  }
+	state = {
+		zoomLvl: 2,
+		zoom: new Animated.Value(0),
+	}
+	componentDidMount() {
+		RCTDeviceEventEmitter.addListener('ondblclick', this.handleDbClick)
+	}
 
-  handleDbClick = e => {
-    const { zoomedIn, zoom } = this.state;
-    Animated.timing(zoom, {
-      toValue: zoomedIn ? 0 : 1,
-      duration: 400,
-    }).start(() => {
-      this.setState(state => ({ zoomedIn: !state.zoomedIn }));
-    });
-  };
+	handleDbClick = e => {
+		const { zoomLvl, zoom } = this.state
+		const toValue = zoomLvl === 6 ? 0 : zoomLvl + 1
+		Animated.timing(zoom, {
+			toValue,
+			duration: 400,
+		}).start(() => {
+			this.setState(state => ({ zoomLvl: toValue }))
+		})
+	}
 
-  render() {
-    const { zoom } = this.state;
-    return (
-      <Galaxy
-        style={{
-          transform: [
-            {
-              translateZ: zoom.interpolate({
-                inputRange: [0, 1],
-                outputRange: [5, 10],
-              }),
-            },
-            {
-              translateY: zoom.interpolate({
-                inputRange: [0, 1],
-                outputRange: [0, 15],
-              }),
-            },
-          ],
-        }}
-      >
-        <Planet
-          texture="mercury.jpg"
-          yearDuration={EARTH_YEAR * 0.62}
-          dayDuration={EARTH_DAY * -243}
-          radius={0.1 * 2.4 / 6.3}
-          elipsisRadius={3 * 0.72}
-          inclination={3.4}
-        />
-        <Planet
-          texture="venus.jpg"
-          yearDuration={EARTH_YEAR * 0.24}
-          dayDuration={EARTH_DAY * 58}
-          radius={0.1 * 6 / 6.3}
-          elipsisRadius={3 * 0.72}
-          inclination={3.4}
-        />
-        <Planet
-          texture="earth.jpg"
-          yearDuration={EARTH_YEAR}
-          dayDuration={EARTH_DAY}
-          radius={0.1}
-          elipsisRadius={3}
-          inclination={7.2}
-        />
-        <Planet
-          texture="mars.jpg"
-          yearDuration={EARTH_YEAR * 1.88}
-          dayDuration={EARTH_DAY * 1.03}
-          radius={0.1 * 3.4 / 6.3}
-          elipsisRadius={3 * 1.52}
-          inclination={1.9}
-        />
-        <Planet
-          texture="jupiter.jpg"
-          yearDuration={EARTH_YEAR * 11.86}
-          dayDuration={EARTH_DAY * 0.4}
-          radius={0.1 * 69.9 / 6.3}
-          elipsisRadius={3 * 5.2}
-          inclination={1.3}
-        />
-        <Planet
-          texture="saturn.jpg"
-          yearDuration={EARTH_YEAR * 29.5}
-          dayDuration={EARTH_DAY * 0.5}
-          radius={0.1 * 58.2 / 6.3}
-          elipsisRadius={3 * 9.6}
-          inclination={2.5}
-        />
-        <Sun />
-      </Galaxy>
-    );
-  }
+	render() {
+		const { zoom, zoomLvl } = this.state
+		return (
+			<Galaxy
+				style={{
+					transform: [
+						{
+							translateZ: zoom.interpolate({
+								inputRange: [0, 1],
+								outputRange: [35, 50],
+							}),
+						},
+						{
+							translateY: zoom.interpolate({
+								inputRange: [0, 1],
+								outputRange: [35, 50],
+							}),
+						},
+						{ rotateX: -45 },
+					],
+				}}
+			>
+				<Planet
+					texture="mercury.jpg"
+					yearDuration={constants.MERCURY_YEAR}
+					dayDuration={constants.MERCURY_DAY}
+					radius={constants.MERCURY_RADIUS}
+					elipsisRadius={constants.MERCURY_AXIS}
+					inclination={constants.MERCURY_INCLINATION}
+				/>
+				<Planet
+					texture="venus.jpg"
+					yearDuration={constants.VENUS_YEAR}
+					dayDuration={constants.VENUS_DAY}
+					radius={constants.VENUS_RADIUS}
+					elipsisRadius={constants.VENUS_AXIS}
+					inclination={constants.VENUS_INCLINATION}
+				/>
+				<Planet
+					texture="earth.jpg"
+					yearDuration={constants.EARTH_YEAR}
+					dayDuration={constants.EARTH_DAY}
+					radius={constants.EARTH_RADIUS}
+					elipsisRadius={constants.EARTH_AXIS}
+					inclination={constants.EARTH_INCLINATION}
+				/>
+				<Planet
+					texture="mars.jpg"
+					yearDuration={constants.MARS_YEAR}
+					dayDuration={constants.MARS_DAY}
+					radius={constants.MARS_RADIUS}
+					elipsisRadius={constants.MARS_AXIS}
+					inclination={constants.MARS_INCLINATION}
+				/>
+				<Planet
+					texture="jupiter.jpg"
+					yearDuration={constants.JUPITER_YEAR}
+					dayDuration={constants.JUPITER_DAY}
+					radius={constants.JUPITER_RADIUS}
+					elipsisRadius={constants.JUPITER_AXIS}
+					inclination={constants.JUPITER_INCLINATION}
+				/>
+				<Planet
+					texture="saturn.jpg"
+					yearDuration={constants.SATURN_YEAR}
+					dayDuration={constants.SATURN_DAY}
+					radius={constants.SATURN_RADIUS}
+					elipsisRadius={constants.SATURN_AXIS}
+					inclination={constants.SATURN_INCLINATION}
+				/>
+				<Planet
+					texture="uranus.jpg"
+					yearDuration={constants.URANUS_YEAR}
+					dayDuration={constants.URANUS_DAY}
+					radius={constants.URANUS_RADIUS}
+					elipsisRadius={constants.URANUS_AXIS}
+					inclination={constants.URANUS_INCLINATION}
+				/>
+				<Planet
+					texture="neptune.jpg"
+					yearDuration={constants.NEPTUNE_YEAR}
+					dayDuration={constants.NEPTUNE_DAY}
+					radius={constants.NEPTUNE_RADIUS}
+					elipsisRadius={constants.NEPTUNE_AXIS}
+					inclination={constants.NEPTUNE_INCLINATION}
+				/>
+				<Sun />
+			</Galaxy>
+		)
+	}
 }
 
-export default App;
+export default App
