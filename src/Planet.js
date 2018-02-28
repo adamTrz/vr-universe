81ce77e8a6f7e77d5f4b980318eee3c8e56f1c1f
Adam Trzciński <trzcinski.adam@gmail.com>
2018-02-28 16:14:25 +0100

    Done some cleanup

--- src/Planet.js
+++ src/Planet.js
@@ -1,126 +1,74 @@
-import React from 'react'
-import { Animated, Sphere, View, asset } from 'react-vr'
-import { Easing } from 'react-native'
-import AnimatedMath from 'react-native-animated-math'
+import React from 'react';
+import { Animated, Sphere, View, asset } from 'react-vr';
+import { Easing } from 'react-native';
+import AnimatedMath from 'react-native-animated-math';
 
-const Globe = Animated.createAnimatedComponent(Sphere)
-
-const radians = degrees => degrees * Math.PI / 180
+const Globe = Animated.createAnimatedComponent(Sphere);
 
 export default class Planet extends React.Component {
-	spin = new Animated.Value(0)
-	// rotation = new Animated.Value(0)
-	inclination = new Animated.Value(0)
-	position = new Animated.ValueXY()
+  spin = new Animated.Value(0);
+  rotation = new Animated.Value(0);
 
-	componentDidMount() {
-		this.startSpinning()
-		this.startRotation()
-	}
+  componentDidMount() {
+    this.startSpinning();
+    this.startRotation();
+  }
 
-	startSpinning = () => {
-		Animated.timing(this.spin, {
-			toValue: 360,
-			easing: Easing.linear,
-			duration: this.props.dayDuration,
-		}).start(() => {
-			this.spin.setValue(0)
-			this.startSpinning()
-		})
-	}
+  startSpinning = () => {
+    Animated.loop(
+      Animated.timing(this.spin, {
+        toValue: 360,
+        easing: Easing.linear,
+        duration: this.props.dayDuration,
+      })
+    ).start();
+  };
 
-	startRotation = () => {
-		Animated.parallel([
-			Animated.timing(this.inclination, {
-				toValue: 360,
-				duration: this.props.yearDuration,
-				easing: Easing.linear,
-			}),
-			Animated.timing(this.position, {
-				toValue: { x: 360, y: 360 },
-				duration: this.props.yearDuration,
-				easing: Easing.linear,
-				extrapolate: 'clamp',
-			}),
-		]).start(() => {
-			this.inclination.setValue(0)
-			this.position.setValue({ x: 0, y: 0 })
-			this.startRotation()
-		})
-		// Animated.timing(this.rotation, {
-		// 	toValue: 2 * Math.PI,
-		// 	easing: Easing.linear,
-		// 	duration: this.props.yearDuration,
-		// }).start(() => {
-		// 	this.rotation.setValue(0)
-		// 	this.startRotation()
-		// })
-	}
+  startRotation = () => {
+    Animated.loop(
+      Animated.timing(this.rotation, {
+        toValue: 2 * Math.PI,
+        easing: Easing.linear,
+        duration: this.props.yearDuration,
+      })
+    ).start();
+  };
 
-	render() {
-		const { elipsisRadius, radius, inclination } = this.props
-		let range = new Array(360)
-		for (let i = 0; i < 360; i++) {
-			range[i] = i
-		}
-		return (
-			<View>
-				<Globe
-					style={{
-						transform: [
-							// {
-							// 	translateX: Animated.multiply(
-							// 		AnimatedMath.sinus(this.rotation),
-							// 		-elipsisRadius
-							// 	),
-							// },
-							// {
-							// 	translateZ: Animated.multiply(
-							// 		AnimatedMath.cosinus(this.rotation),
-							// 		elipsisRadius
-							// 	),
-							// },
-							// {
-							// 	translateY: Animated.multiply(
-							// 		AnimatedMath.sinus(this.rotation),
-							// 		elipsisRadius * Math.sin(inclination)
-							// 	),
-							// },
-							/** without AnimatedMath, take 1: */
-							{
-								translateX: this.position.x.interpolate({
-									inputRange: range,
-									outputRange: range.map(
-										i => -elipsisRadius * Math.sin(i * Math.PI / 180)
-									),
-								}),
-							},
-							{
-								translateZ: this.position.y.interpolate({
-									inputRange: range,
-									outputRange: range.map(
-										i => -elipsisRadius * Math.cos(i * Math.PI / 180)
-									),
-								}),
-							},
-							{
-								translateY: this.inclination.interpolate({
-									inputRange: range,
-									outputRange: range.map(
-										i => inclination * Math.sin(i * Math.PI / 180)
-									),
-								}),
-							},
-							{ rotateY: this.spin },
-						],
-					}}
-					radius={radius}
-					widthSegments={radius * 10 + 20}
-					heightSegments={radius * 10 + 20}
-					lit
-					texture={asset(this.props.texture)}
-				/>
-			</View>
-		)
-	}
+  render() {
+    const { elipsisRadius, radius, inclination } = this.props;
+    return (
+      <View>
+        <Globe
+          style={{
+            transform: [
+              {
+                translateX: Animated.multiply(
+                  AnimatedMath.sinus(this.rotation),
+                  -elipsisRadius
+                ),
+              },
+              {
+                translateZ: Animated.multiply(
+                  AnimatedMath.cosinus(this.rotation),
+                  elipsisRadius
+                ),
+              },
+              {
+                translateY: Animated.multiply(
+                  AnimatedMath.sinus(this.rotation),
+                  elipsisRadius * Math.sin(inclination)
+                ),
+              },
+              { rotateY: this.spin },
+            ],
+          }}
+          radius={radius}
+          widthSegments={radius * 10 + 20}
+          heightSegments={radius * 10 + 20}
+          lit
+          texture={asset(this.props.texture)}
+        />
+      </View>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,125 +1,125 @@
-import * as React from 'react'
-import { View, Scene, PointLight, Animated } from 'react-vr'
+import * as React from 'react';
+import { View, Scene, PointLight, Animated } from 'react-vr';
 
-const Galaxy = Animated.createAnimatedComponent(Scene)
+const Galaxy = Animated.createAnimatedComponent(Scene);
 
-const RCTDeviceEventEmitter = require('RCTDeviceEventEmitter')
+const RCTDeviceEventEmitter = require('RCTDeviceEventEmitter');
 
-import Planet from './Planet'
-import Sun from './Sun'
+import Planet from './Planet';
+import Sun from './Sun';
 
-import * as constants from './constants'
+import * as constants from './constants';
 
 class App extends React.Component {
-	state = {
-		zoomLvl: 2,
-		zoom: new Animated.Value(0),
-	}
-	componentDidMount() {
-		RCTDeviceEventEmitter.addListener('ondblclick', this.handleDbClick)
-	}
+  state = {
+    zoomLvl: 2,
+    zoom: new Animated.Value(0),
+  };
+  componentDidMount() {
+    RCTDeviceEventEmitter.addListener('ondblclick', this.handleDbClick);
+  }
 
-	handleDbClick = e => {
-		const { zoomLvl, zoom } = this.state
-		const toValue = zoomLvl === 6 ? 0 : zoomLvl + 1
-		Animated.timing(zoom, {
-			toValue,
-			duration: 400,
-		}).start(() => {
-			this.setState(state => ({ zoomLvl: toValue }))
-		})
-	}
+  handleDbClick = e => {
+    const { zoomLvl, zoom } = this.state;
+    const toValue = zoomLvl === 6 ? 0 : zoomLvl + 1;
+    Animated.timing(zoom, {
+      toValue,
+      duration: 400,
+    }).start(() => {
+      this.setState(state => ({ zoomLvl: toValue }));
+    });
+  };
 
-	render() {
-		const { zoom, zoomLvl } = this.state
-		return (
-			<Galaxy
-				style={{
-					transform: [
-						{
-							translateZ: zoom.interpolate({
-								inputRange: [0, 1],
-								outputRange: [35, 50],
-							}),
-						},
-						{
-							translateY: zoom.interpolate({
-								inputRange: [0, 1],
-								outputRange: [35, 50],
-							}),
-						},
-						{ rotateX: -45 },
-					],
-				}}
-			>
-				<Planet
-					texture="mercury.jpg"
-					yearDuration={constants.MERCURY_YEAR}
-					dayDuration={constants.MERCURY_DAY}
-					radius={constants.MERCURY_RADIUS}
-					elipsisRadius={constants.MERCURY_AXIS}
-					inclination={constants.MERCURY_INCLINATION}
-				/>
-				<Planet
-					texture="venus.jpg"
-					yearDuration={constants.VENUS_YEAR}
-					dayDuration={constants.VENUS_DAY}
-					radius={constants.VENUS_RADIUS}
-					elipsisRadius={constants.VENUS_AXIS}
-					inclination={constants.VENUS_INCLINATION}
-				/>
-				<Planet
-					texture="earth.jpg"
-					yearDuration={constants.EARTH_YEAR}
-					dayDuration={constants.EARTH_DAY}
-					radius={constants.EARTH_RADIUS}
-					elipsisRadius={constants.EARTH_AXIS}
-					inclination={constants.EARTH_INCLINATION}
-				/>
-				<Planet
-					texture="mars.jpg"
-					yearDuration={constants.MARS_YEAR}
-					dayDuration={constants.MARS_DAY}
-					radius={constants.MARS_RADIUS}
-					elipsisRadius={constants.MARS_AXIS}
-					inclination={constants.MARS_INCLINATION}
-				/>
-				<Planet
-					texture="jupiter.jpg"
-					yearDuration={constants.JUPITER_YEAR}
-					dayDuration={constants.JUPITER_DAY}
-					radius={constants.JUPITER_RADIUS}
-					elipsisRadius={constants.JUPITER_AXIS}
-					inclination={constants.JUPITER_INCLINATION}
-				/>
-				<Planet
-					texture="saturn.jpg"
-					yearDuration={constants.SATURN_YEAR}
-					dayDuration={constants.SATURN_DAY}
-					radius={constants.SATURN_RADIUS}
-					elipsisRadius={constants.SATURN_AXIS}
-					inclination={constants.SATURN_INCLINATION}
-				/>
-				<Planet
-					texture="uranus.jpg"
-					yearDuration={constants.URANUS_YEAR}
-					dayDuration={constants.URANUS_DAY}
-					radius={constants.URANUS_RADIUS}
-					elipsisRadius={constants.URANUS_AXIS}
-					inclination={constants.URANUS_INCLINATION}
-				/>
-				<Planet
-					texture="neptune.jpg"
-					yearDuration={constants.NEPTUNE_YEAR}
-					dayDuration={constants.NEPTUNE_DAY}
-					radius={constants.NEPTUNE_RADIUS}
-					elipsisRadius={constants.NEPTUNE_AXIS}
-					inclination={constants.NEPTUNE_INCLINATION}
-				/>
-				<Sun />
-			</Galaxy>
-		)
-	}
+  render() {
+    const { zoom, zoomLvl } = this.state;
+    return (
+      <Galaxy
+        style={{
+          transform: [
+            {
+              translateZ: zoom.interpolate({
+                inputRange: [0, 1],
+                outputRange: [35, 50],
+              }),
+            },
+            {
+              translateY: zoom.interpolate({
+                inputRange: [0, 1],
+                outputRange: [35, 50],
+              }),
+            },
+            { rotateX: -45 },
+          ],
+        }}
+      >
+        <Planet
+          texture="mercury.jpg"
+          yearDuration={constants.MERCURY_YEAR}
+          dayDuration={constants.MERCURY_DAY}
+          radius={constants.MERCURY_RADIUS}
+          elipsisRadius={constants.MERCURY_AXIS}
+          inclination={constants.MERCURY_INCLINATION}
+        />
+        <Planet
+          texture="venus.jpg"
+          yearDuration={constants.VENUS_YEAR}
+          dayDuration={constants.VENUS_DAY}
+          radius={constants.VENUS_RADIUS}
+          elipsisRadius={constants.VENUS_AXIS}
+          inclination={constants.VENUS_INCLINATION}
+        />
+        <Planet
+          texture="earth.jpg"
+          yearDuration={constants.EARTH_YEAR}
+          dayDuration={constants.EARTH_DAY}
+          radius={constants.EARTH_RADIUS}
+          elipsisRadius={constants.EARTH_AXIS}
+          inclination={constants.EARTH_INCLINATION}
+        />
+        <Planet
+          texture="mars.jpg"
+          yearDuration={constants.MARS_YEAR}
+          dayDuration={constants.MARS_DAY}
+          radius={constants.MARS_RADIUS}
+          elipsisRadius={constants.MARS_AXIS}
+          inclination={constants.MARS_INCLINATION}
+        />
+        <Planet
+          texture="jupiter.jpg"
+          yearDuration={constants.JUPITER_YEAR}
+          dayDuration={constants.JUPITER_DAY}
+          radius={constants.JUPITER_RADIUS}
+          elipsisRadius={constants.JUPITER_AXIS}
+          inclination={constants.JUPITER_INCLINATION}
+        />
+        <Planet
+          texture="saturn.jpg"
+          yearDuration={constants.SATURN_YEAR}
+          dayDuration={constants.SATURN_DAY}
+          radius={constants.SATURN_RADIUS}
+          elipsisRadius={constants.SATURN_AXIS}
+          inclination={constants.SATURN_INCLINATION}
+        />
+        <Planet
+          texture="uranus.jpg"
+          yearDuration={constants.URANUS_YEAR}
+          dayDuration={constants.URANUS_DAY}
+          radius={constants.URANUS_RADIUS}
+          elipsisRadius={constants.URANUS_AXIS}
+          inclination={constants.URANUS_INCLINATION}
+        />
+        <Planet
+          texture="neptune.jpg"
+          yearDuration={constants.NEPTUNE_YEAR}
+          dayDuration={constants.NEPTUNE_DAY}
+          radius={constants.NEPTUNE_RADIUS}
+          elipsisRadius={constants.NEPTUNE_AXIS}
+          inclination={constants.NEPTUNE_INCLINATION}
+        />
+        <Sun />
+      </Galaxy>
+    );
+  }
 }
 
-export default App
+export default App;
